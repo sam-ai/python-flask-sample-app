7a8737aa92ac75719d2ee6305cd15ba34274fa73
sam-ai <34075455+sam-ai@users.noreply.github.com>
2019-02-09 23:03:05 +0530

    Update deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
-docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+#echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin 
+#docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo "$HEROKU_API_KEY" | docker login -u _ --password-stdin registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+#docker push registry.heroku.com/$HEROKU_APP_NAME/web
