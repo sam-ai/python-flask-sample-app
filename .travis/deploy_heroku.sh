97f0037745a5e94054a1bddf50664eff551cbb49
sam-ai <34075455+sam-ai@users.noreply.github.com>
2019-02-09 23:12:19 +0530

    Update deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,5 +4,8 @@ heroku plugins:install heroku-container-registry
 #echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin 
 #docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 echo "$HEROKU_API_KEY" | docker login -u _ --password-stdin registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+docker tag $DOCKER_USER/$APP_NAME:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:web release --app $HEROKU_APP_NAME
+#heroku container:push web --app $HEROKU_APP_NAME
 #docker push registry.heroku.com/$HEROKU_APP_NAME/web
